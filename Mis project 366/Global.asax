0e55d8a4eb1a77a0217cf7ab8733cd4c7eaaf187
fabla <fabla@FAHDAH-ALZEER>
2025-11-12 05:23:47 +0300

    Add project files.

---
+++ Mis project 366/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mis_project_366.Global" Language="C#" %>
---
+++ Mis project 366/Masterpage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mis_project_366
+{
+    public partial class Master_page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
